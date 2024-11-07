135dda7711049eee8bfcadbbc2b8cbb4731c58e8
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-11-07 12:38:25 -0500

    Create GameFrame.java

---
+++ Snake Game/src/snakegame/GameFrame.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snakegame;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Danny
+ */
+public class GameFrame extends JFrame {
+
+    public GameFrame() {
+        GamePanel panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+    }
+    
+}
