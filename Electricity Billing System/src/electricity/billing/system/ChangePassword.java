aed34f70b4a381c9f73536de1bb3f6e66a5101ab
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2025-10-25 15:04:47 -0400

    Create ChangePassword.java

---
+++ Electricity Billing System/src/electricity/billing/system/ChangePassword.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package electricity.billing.system;
+
+/**
+ *
+ * @author danny
+ */
+public class ChangePassword {
+
+    public ChangePassword(String meterNumber) {
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
